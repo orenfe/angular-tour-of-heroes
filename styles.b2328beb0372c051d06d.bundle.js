0f0f85eda2a0bfe18c9db982d071c96e1829151a
orenfe <OrenFe@bezeq.co.il>
2016-12-31 21:31:34 +0200

    deploy step 7 of this project...

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{358:function(n,o){},646:function(n,o,c){n.exports=c(358)}},[646]);
+webpackJsonp([1,2],{382:function(n,o){},691:function(n,o,c){n.exports=c(382)}},[691]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
