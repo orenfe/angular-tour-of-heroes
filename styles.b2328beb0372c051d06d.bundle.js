d2ae602f671f468608f09acd0769a566d4666b6d
orenfe <OrenFe@bezeq.co.il>
2016-12-31 17:21:26 +0200

    Second deploy of this project...

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{330:function(n,o){},597:function(n,o,c){n.exports=c(330)}},[597]);
+webpackJsonp([1,2],{315:function(n,o){},583:function(n,o,c){n.exports=c(315)}},[583]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
